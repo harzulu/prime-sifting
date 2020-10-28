6220e6bbf778c1508ad816c66b80c96c3f3a5f74
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-28 14:52:43 -0700

    Add basics to have input and submit button
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 
+// form submission
+    $("#number").submit((e) => {
+        e.preventDefault();
+    // collect user input
+        let originalNumber = parseInt($("#number-input").val());
+        console.log()
+
+    })
 });
