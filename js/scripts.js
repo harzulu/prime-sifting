e3b8cb30d0ed265dfbc1b2bf4a176081a445a125
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-28 16:34:18 -0700

    clean up code and comments
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,11 @@ function findPrimes(number) {
 }
 
 $(document).ready(function() {
-
-// form submission
     $("#number").submit((e) => {
         e.preventDefault();
-    // collect user input
+
         let originalNumber = parseInt($("#number-input").val());
-        // console.log(typeof originalNumber, originalNumber);
-   
-    // 
+  
         if (originalNumber <= 2) {
             alert("Your number is too small!");
         } else {
@@ -33,26 +29,6 @@ $(document).ready(function() {
           primeNumbers.forEach(function(number) {
             $("#output").append("<li>" + number + "</li>");
           });
-
         }
-
-    // close form submission code
     });
-// end of .ready()
 });
-
-
-    //  if numArr[i] is a factor of 2,3,5,7
-    //    then .filter() factors
-
-    //   
-     // 
-    //         for (i=0; i < numArr.length; i++) {
-        
-    //          if (numArr[i] % 2  && numArr[i] % 3 && numArr[i] % 5 && numArr[i] % 7) {          
-    //            array.push(numArr[i])                                                                  
-    //          } else if (numArr[i} === 2 || numArr[i} === 3 || numArr[i} === 5 || numArr[i} === 7) {
-//                    array.push(numArr[i])              
-//               }           
-    //     }
-    //    })
